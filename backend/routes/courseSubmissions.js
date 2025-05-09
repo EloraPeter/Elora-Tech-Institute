700c254bc84bbda8ba44d2f0f34be41f65e65ca1
elorapeter <florenceonyi09@gmail.com>
2025-05-09 10:34:48 +0000

    May 9, 2025, 11:34 AM

--- backend/routes/courseSubmissions.js
+++ backend/routes/courseSubmissions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('../db');
-const { authenticateJWT } = require('../auth'); // Import authenticateJWT from auth.js
+const { authenticateJWT } = require('../auth');
 
 const router = express.Router();
 
