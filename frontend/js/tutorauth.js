72e83bce22779734aa7cc83b0024476adb7d401d
elorapeter <florenceonyi09@gmail.com>
2025-05-13 10:53:38 +0000

    May 13, 2025, 11:53 AM

--- frontend/js/tutorauth.js
+++ frontend/js/tutorauth.js
@@ -9,7 +9,7 @@ async function signup() {
     const name = document.getElementById("signupName").value;
     const email = document.getElementById("signupEmail").value;
     const password = document.getElementById("signupPassword").value;
-    const role = document.getElementById("signupRole").value;
+    const role = "instructor";
     const signupError = document.getElementById("signupError");
 
     try {
